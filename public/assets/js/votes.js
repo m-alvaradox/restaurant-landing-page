577f4eddc9c6a07aae1e489d7293f484753aea76
Mario Alvarado <m_alvaradox@hotmail.com>
2025-06-18 14:01:28 -0500

    final / Update votes

--- public/assets/js/votes.js
+++ public/assets/js/votes.js
@@ -1,28 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getDatabase, ref, set, push, get } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyCFjHWbDSG5vDCjElsuXelZ7DhQaRHGPhg",
+  authDomain: "restaurant-landing-page-75167.firebaseapp.com",
+  databaseURL: "https://restaurant-landing-page-75167-default-rtdb.firebaseio.com",
+  projectId: "restaurant-landing-page-75167",
+  storageBucket: "restaurant-landing-page-75167.firebasestorage.app",
+  messagingSenderId: "705888294177",
+  appId: "1:705888294177:web:553583fceea64f24bf9a60"
 };
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.database();
 
-  document.querySelectorAll(".like-btn").forEach(btn => {
-    const id = btn.dataset.id;
-    const countSpan = btn.querySelector(".like-count");
-    const ref = db.ref("likes/" + id);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
-    ref.on("value", snapshot => {
-      countSpan.textContent = snapshot.val() || 0;
-    });
+document.querySelectorAll(".like-btn").forEach(btn => {
+  const id = btn.dataset.id;
+  const countSpan = btn.querySelector(".like-count");
+  const ref = db.ref("likes/" + id);
 
-    btn.addEventListener("click", () => {
-      ref.transaction(current => (current || 0) + 1);
-    });
+  ref.on("value", snapshot => {
+    countSpan.textContent = snapshot.val() || 0;
   });
+
+  btn.addEventListener("click", () => {
+    ref.transaction(current => (current || 0) + 1);
+  });
+});
